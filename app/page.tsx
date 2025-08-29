f0f72b2496a597b948cdcd8719e1f507841497d1
Avendaño <chicopalodelacruz0381@gmail.com>
2025-08-29 17:08:48 -0500

    corriguiendo un error

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="bg-red-300 p-96"></div>
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-
-      <div className="bg-red-300 p-96"></div>
-      <div className="bg-red-300 p-96"></div>
-      <div className="bg-red-300 p-96"></div>
-      <div className="bg-red-300 p-96"></div>
-      <div className="bg-red-300 p-96"></div>
+      
     </div>
   );
 }
